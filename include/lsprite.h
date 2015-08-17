90469fb41f025e631cf82eab2762b068fa53be33
Arthur Dent <arthurdent475@gmail.com>
2015-08-18 00:37:53 +0200

    ADD : lsprite

---
+++ include/lsprite.h
@@ -0,0 +1,21 @@
+
+#ifndef _LSPRITE_H_
+# define _LSPRITE_H_
+
+#include <memory>
+#include "ltexture.h"
+#include <SDL2/SDL.h>
+
+class	LSprite {
+	public:
+		LSprite(std::shared_ptr<LTexture*> tex, int x, int y);
+
+		void	render();
+
+	protected:
+		std::shared_ptr<LTexture*>	mTex;
+		int	mX;
+		int	mY;
+};
+
+#endif /* !_LSPRITE_H_ */
---
+++ src/lsprite.cpp
@@ -0,0 +1,8 @@
+#include "lsprite.h"
+
+LSprite::LSprite(std::shared_ptr<LTexture*> tex, int x, int y) :
+	mTex(tex), mX(x), mY(y) {}
+
+void	LSprite::render() {
+	(*mTex)->render(mX, mY);
+}
